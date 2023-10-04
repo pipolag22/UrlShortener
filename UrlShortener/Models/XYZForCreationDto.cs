4bde0501d22c4bb549471e5e5cd8ee43cef7fb32
pipolag <jorgebellerate@gmail.com>
2023-10-04 08:22:46 -0300

    haciendo de a poco

---
+++ UrlShortener/Models/XYZForCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UrlShortener.Models
+{
+    public class XYZForCreationDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; } = "";
+        public string ShortUrl { get; set; } = "";
+    }
+}
