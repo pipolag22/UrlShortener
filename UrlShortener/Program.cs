bd31f4a69468d58f0158353bd2cff797c4aa0d96
pipolag <jorgebellerate@gmail.com>
2023-11-20 12:03:58 -0300

    .

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,3 +1,8 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+using UrlShortener.Data;
+using UrlShortener.Services;
+
 namespace UrlShortener
 {
     public class Program
@@ -8,10 +13,64 @@ namespace UrlShortener
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            //builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                options.JsonSerializerOptions.WriteIndented = true;
+            });
+
+
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            //builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(setupAction =>
+            {
+                setupAction.AddSecurityDefinition("UrlApiBearerAuth", new OpenApiSecurityScheme() //Esto va a permitir usar swagger con el token.
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    Description = "escribir token para logearse."
+                });
+
+                setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                            {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "UrlApiBearerAuth" } //Tiene que coincidir con el id seteado arriba en la definici�n
+                            }, new List<string>() }
+                });
+            });
+
+
+            #region DependencyInjections
+            builder.Services.AddScoped<UrlServices>();
+            builder.Services.AddScoped<UserServices>();
+            #endregion
+
+            builder.Services.AddDbContext<UrlShortenerContext>(dbContextOptions => dbContextOptions.UseSqlite(
+                builder.Configuration["ConnectionStrings:shorter1DBConnectionString"]));
+
+            builder.Services.AddAuthentication("Bearer") //"Bearer" es el tipo de auntenticaci�n que tenemos que elegir despu�s en PostMan para pasarle el token
+            .AddJwtBearer(options => //Ac� definimos la configuraci�n de la autenticaci�n. le decimos qu� cosas queremos comprobar. La fecha de expiraci�n se valida por defecto.
+            {
+                options.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Authentication:Issuer"],
+                    ValidAudience = builder.Configuration["Authentication:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Authentication:SecretForKey"]))
+                };
+            }
+);
+
 
             var app = builder.Build();
 
@@ -22,8 +81,13 @@ namespace UrlShortener
                 app.UseSwaggerUI();
             }
 
+            app.UseCors(
+            options => options.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin()
+            );
+
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
